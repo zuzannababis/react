a578afd3b4dbe549040feab547d064f0c3cca631
Zuza Babis <zuzanna.babis@gmail.com>
2020-02-09 17:36:36 +0100

    Install shortid

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import {combineReducers, createStore} from 'redux';
-import initialStoreData from '../src/data/dataStore.js';
+import initialStoreData from '../data/dataStore.js';
 
 // define initial state and shallow-merge initial data
 const initialState = {
